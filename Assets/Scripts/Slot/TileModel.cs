d99e9c5e28459d599838261686aa5ca2883a01ab
andrey.s <andrey.s@idx-zero.com>
2024-01-08 18:49:13 +0300

    Unique ID added as string to int

--- Assets/Scripts/Slot/TileModel.cs
+++ Assets/Scripts/Slot/TileModel.cs
@@ -14,17 +14,7 @@ namespace Match3.Models
         private UniqueID _uniqueID = null;
         public UniqueID ID
         {
-            get
-            {
-                Debug.Log($"{_uniqueID} == {null}");
-                var id0 = new UniqueID("111");
-                var id2 = new UniqueID("222");
-                var t = id0 == id2;
-                if (_uniqueID == null)
-                    _uniqueID = new UniqueID(_id);
-
-                return _uniqueID;
-            }
+            get { return _uniqueID ??= new UniqueID(_id); }
         }
         public SpriteAnimationObject SpriteAnimation => _spriteAnimation;
     }
